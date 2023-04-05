aa4dd7b52e25ebbccb03bae3390b27eb8a597f1d
Harshitaa M S <88045719+msharshitaa@users.noreply.github.com>
2023-04-05 23:38:24 +0530

    Add files via upload

---
+++ js/profile.js
@@ -0,0 +1,16 @@
+function calculateAge() {
+    const dobElement = document.getElementById("dob");
+    const ageElement = document.getElementById("age");
+  
+    const dobValue = new Date(dobElement.value);
+    const today = new Date();
+  
+    let age = today.getFullYear() - dobValue.getFullYear();
+    const monthDiff = today.getMonth() - dobValue.getMonth();
+    
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dobValue.getDate())) {
+        age--;
+    }
+  
+    ageElement.value = age;
+  }
